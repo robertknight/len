216e5a51eeb21f9d2a2f1334a14cc60012662311
Robert Knight <robertknight@gmail.com>
2021-12-16 23:40:12 +0000

    Optimize by reading input in chunks
    Read input in 4KB chunks instead of one byte at a time, which is much
    faster.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,25 @@ use atty::Stream;
 use bytesize;
 use std::io::Read;
 
-fn main() {
-    let len = std::io::stdin().bytes().count();
-    let human = atty::is(Stream::Stdout);
+fn main() -> Result<(), std::io::Error> {
+    let mut buf: [u8; 4096] = [0; 4096];
+    let mut stdin = std::io::stdin();
+    let mut len = 0;
 
+    loop {
+        match stdin.read(&mut buf) {
+            Ok(0) => break,
+            Ok(n) => len += n,
+            Err(e) => return Err(e),
+        }
+    }
+
+    let human = atty::is(Stream::Stdout);
     if human {
         println!("{}", bytesize::to_string(len as u64, true));
     } else {
         println!("{}", len);
     }
+
+    Ok(())
 }
